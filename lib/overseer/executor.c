8153fc467c8dba8782c22f67b4bc31bcd3aca9de
Yonathan Cahyadi <project.cahyadiyonathan@gmail.com>
2020-10-24 01:41:05 +1000

    Added child process SIGNAL handling

--- lib/overseer/executor.c
+++ lib/overseer/executor.c
@@ -19,6 +19,40 @@
 #include "../global/macro.h"
 
 
+void check_exec(int pid, request_queue_node req_node){
+	int child_status_code;
+	/** check child status */
+	sleep(1);
+	if(waitpid(pid, &child_status_code, WNOHANG) == 0){ /** successfully running */
+		print_log(stdout, "%s has been executed with pid %d", req_node.req->arguments, pid);
+	}
+}
+
+void child_process_signaling(int pid, request_queue_node req_node){
+	
+	int child_status_code = 0;
+	
+	/** child process signaling */
+	if(req_node.req->t_flag){
+		sleep(req_node.req->seconds);
+	}else{
+		sleep(10);
+	}
+	if(waitpid(pid, &child_status_code, WNOHANG) == 0){/** child process still exist */
+		kill(pid, SIGTERM);
+		print_log(stdout, "sent SIGTERM to %d", pid);
+		sleep(5);
+		if(waitpid(pid, &child_status_code, WNOHANG) == 0){/** child process still exist */
+			kill(pid, SIGKILL);
+			print_log(stdout, "sent SIGKILL to %d", pid);
+		}else{
+			print_log(stdout, "%d has terminated with status code %d", pid, WEXITSTATUS(child_status_code));
+		}
+	}else{
+		print_log(stdout, "%d has terminated with status code %d", pid, WEXITSTATUS(child_status_code));
+	}
+}
+
 void request_exec(request_queue_node req_node, pthread_mutex_t pro_mutex) {
 	/** check if log flag is on, if it's on redirect the logging */
 	int terminal_fd[2]; /** saving the terminal file descriptor */
@@ -40,98 +74,97 @@ void request_exec(request_queue_node req_node, pthread_mutex_t pro_mutex) {
 	/** log execution attempt */
 	print_log(stdout, "attempting to execute %s", req_node.req->arguments);
 
-	/** fork process */
-	pid_t pid = fork();
-	if (pid == 0) { /** child process */
-		/** split string of arguments into array of string */
-		char temp_arg[sizeof(req_node.req->arguments)]; /** make a copy of req.arguments */
-		CLEAR_CHAR_BUFFER(temp_arg, sizeof(temp_arg));
-		strncpy(temp_arg, req_node.req->arguments, sizeof(temp_arg));
 
-		/** calculate the number of string available */
-		int num_of_string = 0;
-		char* token = strtok(temp_arg, " ");
-		while (token) {
-			num_of_string++;
-			token = strtok(NULL, " ");
+	/** split string of arguments into array of string */
+	char temp_arg[sizeof(req_node.req->arguments)]; /** make a copy of req.arguments */
+	CLEAR_CHAR_BUFFER(temp_arg, sizeof(temp_arg));
+	strncpy(temp_arg, req_node.req->arguments, sizeof(temp_arg));
+
+	/** calculate the number of string available */
+	int num_of_string = 0;
+	char* token = strtok(temp_arg, " ");
+	while (token) {
+		num_of_string++;
+		token = strtok(NULL, " ");
+	}
+
+	/** begin splitting req_node.req->arguments */
+	char* result_argv[num_of_string + 1];
+	CLEAR_CHAR_BUFFER(temp_arg, sizeof(temp_arg));
+	strncpy(temp_arg, req_node.req->arguments, sizeof(temp_arg));
+
+	/** split arguments and store it in result_argv */
+	int i = 0;
+	char* token2 = strtok(temp_arg, " ");
+	while (token2) {
+		result_argv[i] = token2;
+		i++;
+		token2 = strtok(NULL, " ");
+	}
+	/** add null to the end of result_argv */
+	result_argv[num_of_string] = NULL;
+
+
+	/** check if the requested arguments file exist */
+	if(access(result_argv[0], F_OK) != -1){ /** requested arguments exist */
+		/** fork process */
+		pid_t pid = fork();
+		if (pid == 0) { /** child process */
+			
+			/** if log flag is on return the STDOUT and STDERR to the child terminal */
+			if (req_node.req->log_flag) {
+				dup2(terminal_fd[0], STDOUT_FILENO);
+				dup2(terminal_fd[1], STDERR_FILENO);
+			}
+
+			/** redirect ouput if o_flag is on */
+			if (req_node.req->o_flag) {
+				/** create dir if not exist */
+				create_dir(req_node.req->out_file_path);
+				/** create the file */
+				int file_fd = CHECK(open(req_node.req->out_file_path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR), "error creating output file (%s)", strerror(errno));
+				/** redirect stdout and stderr to the file*/
+				dup2(file_fd, STDOUT_FILENO);
+				dup2(file_fd, STDERR_FILENO);
+				/** close the file, file descriptor */
+				close(file_fd);
+			}
+
+			/** replace child proceses with the new proceses */
+			EXEC(execvp, result_argv, print_log, req_node.req->arguments);
+			/** any argument past this, will not be executed */
+
+		} else if (pid > 0) { /** parrent process */
+
+			/** store child PID in the process pool */ 
+			pthread_mutex_lock(&pro_mutex);
+			queue_process(pid, req_node.req->arguments);
+			pthread_mutex_unlock(&pro_mutex);
+
+
+			/** check exec status */
+			check_exec(pid, req_node);
+
+			/** Child signaling */
+			child_process_signaling(pid, req_node);
+
+			/** remove child PID in the process pool */
+			pthread_mutex_lock(&pro_mutex);
+			
+			pthread_mutex_unlock(&pro_mutex);
+			
+
+		} else { /** fork failed */
+			print_log(stderr, "fork failed (%s)", strerror(errno));
 		}
+	}else{/** requested arguments doesn't exist */
+		print_log(stdout, "could not execute %s", req_node.req->arguments);
+	}
 
-		/** begin splitting req.arguments */
-		char* result_argv[num_of_string + 1];
-		CLEAR_CHAR_BUFFER(temp_arg, sizeof(temp_arg));
-		strncpy(temp_arg, req_node.req->arguments, sizeof(temp_arg));
-
-		/** split arguments and store it in result_argv */
-		int i = 0;
-		char* token2 = strtok(temp_arg, " ");
-		while (token2) {
-			result_argv[i] = token2;
-			i++;
-			token2 = strtok(NULL, " ");
-		}
-		/** add null to the end of result_argv */
-		result_argv[num_of_string] = NULL;
-
-		/** if log flag is on return the STDOUT and STDERR to the child terminal */
-		if (req_node.req->log_flag) {
-			dup2(terminal_fd[0], STDOUT_FILENO);
-			dup2(terminal_fd[1], STDERR_FILENO);
-		}
-
-		/** redirect ouput if o_flag is on */
-		if (req_node.req->o_flag) {
-			/** create dir if not exist */
-			create_dir(req_node.req->out_file_path);
-			/** create the file */
-			int file_fd = CHECK(open(req_node.req->out_file_path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR), "error creating output file (%s)", strerror(errno));
-			/** redirect stdout and stderr to the file*/
-			dup2(file_fd, STDOUT_FILENO);
-			dup2(file_fd, STDERR_FILENO);
-			/** close the file, file descriptor */
-			close(file_fd);
-		}
-
-		/** replace child proceses with the new proceses */
-		EXEC(execvp, result_argv, print_log, req_node.req->arguments);
-		/** any argument past this, will not be executed */
-
-	} else if (pid > 0) { /** parrent process */
-
-
-        /** store child PID in the process pool */ 
-		pthread_mutex_lock(&pro_mutex);
-        queue_process(pid, req_node.req->arguments);
-		pthread_mutex_unlock(&pro_mutex);
-
-
-
-		/** check if child exist */
-		int child_status_code = 0;
-		/** send SIGTERM to Child process */
-		
-		/** check child status */
-		sleep(1);
-		if(waitpid(pid, &child_status_code, WNOHANG) == 0){ /** successfully running */
-			print_log(stdout, "%s has been executed with pid %d", req_node.req->arguments, pid);
-		}
-
-		/** wait for child process to terminate */
-		
-		
-		// waitpid(pid, &child_status_code, 0); /** blocking until child terminate*/
-		/** log child termination */
-		print_log(stdout, "%d has terminated with status code %d", pid, WEXITSTATUS(child_status_code));
-
-        
-
-		/** if log flag is on return the STDOUT and STDERR to the parent terminal */
-		if (req_node.req->log_flag) {
-			dup2(terminal_fd[0], STDOUT_FILENO);
-			dup2(terminal_fd[1], STDERR_FILENO);
-		}
-
-	} else { /** fork failed */
-		print_log(stderr, "fork failed (%s)", strerror(errno));
+	/** if log flag is on return the STDOUT and STDERR to the parent terminal */
+	if (req_node.req->log_flag) {
+		dup2(terminal_fd[0], STDOUT_FILENO);
+		dup2(terminal_fd[1], STDERR_FILENO);
 	}
 }
 
@@ -145,7 +178,6 @@ void kill_all_child(pthread_mutex_t pro_mutex){
 			kill(tmp->pid, SIGKILL);
 		}
 	}
-
 	pthread_mutex_unlock(&pro_mutex);
 }
 
